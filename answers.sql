afeb9a52f5211efc6225bcbb5f76154779452345
AymenBida <aymen.bida.00@gmail.com>
2021-01-27 16:59:40 +0100

    Complete a part of exercice 3

--- answers.sql
+++ answers.sql
@@ -38,3 +38,30 @@ SELECT capital, name FROM world WHERE capital LIKE concat('%', name, '%') AND ca
 
 SELECT name, REPLACE(capital, name, '') FROM world WHERE capital LIKE concat('%', name, '%') AND capital != name;
 
+--Exercice 3:
+
+SELECT name, continent, population FROM world;
+
+SELECT name FROM world WHERE population > 200000000;
+
+SELECT name, GDP / population AS 'per capita GDP' FROM world WHERE population > 200000000;
+
+SELECT name, population / 1000000 AS population FROM world where continent = 'South America';
+
+SELECT name, population FROM world WHERE name IN ('France', 'Germany', 'Italy');
+
+SELECT name FROM world WHERE name LIKE '%United%';
+
+SELECT name, population, area FROM world WHERE population > 250000000 OR area > 3000000;
+
+SELECT name, population, area FROM world WHERE population > 250000000 XOR area > 3000000;
+
+SELECT
+  name,
+  ROUND((population / 1000000), 2) AS population,
+  ROUND((gdp / 1000000000), 2) AS gdp
+  FROM world
+  WHERE continent = 'South America';
+
+SELECT name, ROUND((gdp / population) / 1000) * 1000 AS 'per capita GDP' FROM world WHERE gdp > 1000000000000;
+
