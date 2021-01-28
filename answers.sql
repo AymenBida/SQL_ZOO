a5e44d2ee2b02047197bebf6ba116be4cbae9bc0
AymenBida <aymen.bida.00@gmail.com>
2021-01-28 10:40:42 +0100

    Pause because of SQLZOO server crash

--- answers.sql
+++ answers.sql
@@ -116,3 +116,23 @@ SELECT winner, subject
 
 --Exercice 5:
 
+SELECT name FROM world WHERE population > (SELECT population FROM world WHERE name='Russia');
+
+SELECT name FROM world
+  WHERE continent = 'Europe'
+  AND gdp / population > (SELECT gdp / population FROM world WHERE name = 'United Kingdom');
+
+SELECT name, continent FROM world
+  WHERE continent IN (
+    (SELECT continent FROM world WHERE name = 'Argentina'),
+    (SELECT continent FROM world WHERE name = 'Australia')
+    );
+
+SELECT name FROM world
+  WHERE population > (SELECT population FROM world WHERE name = 'Canada')
+  AND population < (SELECT population FROM world WHERE name = 'Poland');
+
+SELECT name,
+  CONCAT(ROUND(100 * population / (SELECT population FROM world WHERE name = 'Germany')), '%') AS percentage
+  FROM world WHERE continent = 'Europe';
+
