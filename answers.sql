6385720365ff0eb36d33998d2cf4cd98bc942606
AymenBida <aymen.bida.00@gmail.com>
2021-01-28 16:44:03 +0100

    Complete Exercice 8

--- answers.sql
+++ answers.sql
@@ -208,3 +208,56 @@ SELECT mdate,
 
 --Exercice 8:
 
+SELECT id, title FROM movie WHERE yr = 1962;
+
+SELECT yr FROM movie WHERE title = 'Citizen Kane';
+
+SELECT id, title, yr FROM movie WHERE title LIKE '%Star Trek%' ORDER BY yr;
+
+SELECT id FROM actor WHERE name = 'Glenn Close';
+
+SELECT id FROM movie WHERE title = 'Casablanca';
+
+SELECT name FROM actor JOIN casting ON id = actorid WHERE movieid = 11768;
+
+SELECT name FROM actor JOIN casting ON id = actorid
+  WHERE movieid = (SELECT id FROM movie WHERE title = 'Alien');
+
+SELECT title FROM movie JOIN casting ON id = movieid
+  WHERE actorid = (SELECT id FROM actor WHERE name = 'Harrison Ford');
+
+SELECT title FROM movie JOIN casting ON id = movieid
+  WHERE actorid = (SELECT id FROM actor WHERE name = 'Harrison Ford') AND ord != 1;
+
+SELECT title, name FROM movie
+  JOIN casting ON movie.id = movieid
+  JOIN actor ON actor.id = actorid
+  WHERE yr = 1962 AND ord = 1;
+
+SELECT yr, COUNT(title) FROM movie
+  JOIN casting ON movie.id = movieid
+  JOIN actor   ON actorid = actor.id
+  WHERE name = 'Rock Hudson' GROUP BY yr HAVING COUNT(title) > 2;
+
+SELECT title, name FROM casting
+  JOIN movie ON (movie.id = movieid AND ord = 1)
+  JOIN actor ON actor.id = actorid
+  WHERE movie.id IN (
+    SELECT movieid FROM casting WHERE actorid IN (
+      SELECT id FROM actor WHERE name='Julie Andrews'));
+
+SELECT name FROM actor
+  JOIN casting ON (actor.id = actorid
+                  AND (SELECT COUNT(ord) FROM casting
+                        WHERE actorid = actor.id AND ord = 1) >= 15) GROUP BY name;
+
+SELECT title, COUNT(actorid) FROM movie JOIN casting on id = movieid
+  WHERE yr = 1978 GROUP BY title ORDER BY COUNT(actorid) DESC, title;
+
+SELECT DISTINCT name FROM actor
+  JOIN casting ON actor.id = actorid
+  WHERE name != 'Art Garfunkel'
+  AND movieid IN (SELECT movieid FROM casting
+                    WHERE actorid = (SELECT actor.id FROM actor
+                                      WHERE name = 'Art Garfunkel'));
+
