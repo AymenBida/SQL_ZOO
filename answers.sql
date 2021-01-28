20c0752b193446330a85181c103c3bae390761c8
AymenBida <aymen.bida.00@gmail.com>
2021-01-28 16:59:06 +0100

    Complete Exercice 9

--- answers.sql
+++ answers.sql
@@ -261,3 +261,33 @@ SELECT DISTINCT name FROM actor
                     WHERE actorid = (SELECT actor.id FROM actor
                                       WHERE name = 'Art Garfunkel'));
 
+--Exercice 9:
+
+SELECT name FROM teacher WHERE dept IS NULL;
+
+SELECT teacher.name, dept.name FROM teacher INNER JOIN dept ON (teacher.dept = dept.id);
+
+SELECT teacher.name, dept.name FROM teacher LEFT JOIN dept ON (teacher.dept = dept.id);
+
+SELECT teacher.name, dept.name FROM teacher RIGHT JOIN dept ON (teacher.dept = dept.id);
+
+SELECT name, COALESCE(mobile, '07986 444 2266') FROM teacher;
+
+SELECT teacher.name, COALESCE(dept.name, 'None') FROM teacher LEFT JOIN dept ON (teacher.dept = dept.id);
+
+SELECT COUNT(name), COUNT(mobile) FROM teacher;
+
+SELECT dept.name, COUNT(teacher.name) FROM teacher RIGHT JOIN dept ON (teacher.dept = dept.id) GROUP BY dept.name;
+
+SELECT teacher.name,
+  CASE WHEN (dept.id = 1 OR dept.id = 2) THEN 'Sci'
+  ELSE 'Art' END
+  FROM teacher LEFT JOIN dept ON (teacher.dept = dept.id);
+
+SELECT teacher.name,
+  CASE WHEN (dept.id = 1 OR dept.id = 2) THEN 'Sci'
+  WHEN dept.id = 3 THEN 'Art'
+  ELSE 'None' END
+  FROM teacher LEFT JOIN dept ON (teacher.dept = dept.id);
+
+--Exercice 10:
