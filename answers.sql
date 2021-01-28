c9d1cc873f63c15af13b3b227507cb7d543b826b
AymenBida <aymen.bida.00@gmail.com>
2021-01-28 15:18:45 +0100

    Complete Exercice 7

--- answers.sql
+++ answers.sql
@@ -170,3 +170,41 @@ SELECT continent FROM world GROUP BY continent HAVING SUM(population) >= 1000000
 
 --Exercice 7:
 
+SELECT matchid, player FROM goal WHERE teamid = 'GER';
+
+SELECT id, stadium, team1, team2 FROM game WHERE id = 1012;
+
+SELECT player, teamid, stadium, mdate FROM game JOIN goal ON id = matchid WHERE teamid = 'GER';
+
+SELECT team1, team2, player FROM game JOIN goal ON id = matchid WHERE player LIKE 'Mario%';
+
+SELECT player, teamid, coach, gtime FROM goal JOIN eteam ON teamid = id WHERE gtime <= 10;
+
+SELECT mdate, teamname FROM game JOIN eteam ON team1 = eteam.id WHERE coach = 'Fernando Santos';
+
+SELECT player FROM goal JOIN game ON matchid = id WHERE stadium = 'National Stadium, Warsaw';
+
+SELECT DISTINCT player FROM game JOIN goal ON matchid = id
+  WHERE teamid != 'GER' AND (team1 = 'GER' OR team2 = 'GER');
+
+SELECT teamname, COUNT(teamid) FROM eteam JOIN goal ON id=teamid GROUP BY teamname;
+
+SELECT stadium, COUNT(matchid) FROM game JOIN goal ON id = matchid GROUP BY stadium;
+
+SELECT matchid, mdate, COUNT(matchid) FROM game JOIN goal ON matchid = id 
+  WHERE (team1 = 'POL' OR team2 = 'POL') GROUP BY matchid, mdate;
+
+SELECT matchid, mdate, COUNT(matchid) FROM game JOIN goal ON matchid = id 
+  WHERE teamid = 'GER' GROUP BY matchid, mdate;
+
+SELECT mdate,
+  team1,
+  SUM(CASE WHEN teamid = team1 THEN 1 ELSE 0 END) score1,
+  team2,
+  SUM(CASE WHEN teamid = team2 THEN 1 ELSE 0 END) score2
+  FROM game LEFT JOIN goal ON matchid = id
+  GROUP BY id, mdate, team1, team2
+  ORDER BY mdate, matchid, team1, team2;
+
+--Exercice 8:
+
