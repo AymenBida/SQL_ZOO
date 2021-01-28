deae00b94a306bf194586f754eca39a8f8bcd548
AymenBida <aymen.bida.00@gmail.com>
2021-01-28 11:21:32 +0100

    Continue with Exercice 5

--- answers.sql
+++ answers.sql
@@ -126,7 +126,7 @@ SELECT name, continent FROM world
   WHERE continent IN (
     (SELECT continent FROM world WHERE name = 'Argentina'),
     (SELECT continent FROM world WHERE name = 'Australia')
-    );
+    ) ORDER BY name;
 
 SELECT name FROM world
   WHERE population > (SELECT population FROM world WHERE name = 'Canada')
@@ -135,4 +135,6 @@ SELECT name FROM world
 SELECT name,
   CONCAT(ROUND(100 * population / (SELECT population FROM world WHERE name = 'Germany')), '%') AS percentage
   FROM world WHERE continent = 'Europe';
+
+SELECT name FROM world WHERE gdp > ALL(SELECT gdp FROM world WHERE continent = 'Europe' AND gdp > 0);
 
