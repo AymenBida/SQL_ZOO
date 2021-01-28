f879a37903a690cb18f1e528b8116dd998dce29d
AymenBida <aymen.bida.00@gmail.com>
2021-01-28 09:25:49 +0100

    Continue with Exercice 4

--- answers.sql
+++ answers.sql
@@ -77,3 +77,40 @@ SELECT name FROM world
   AND name LIKE '%u%'
   AND name NOT LIKE '% %';
 
+-- Exercice 4:
+
+SELECT yr, subject, winner FROM nobel WHERE yr = 1950;
+
+SELECT winner FROM nobel WHERE yr = 1962 AND subject = 'Literature';
+
+SELECT yr, subject FROM nobel WHERE winner = 'Albert Einstein';
+
+SELECT winner FROM nobel WHERE subject = 'Peace' AND yr >= 2000;
+
+SELECT * FROM nobel WHERE subject = 'Literature' AND yr >= 1980 AND yr <= 1989;
+
+SELECT * FROM nobel WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama');
+
+SELECT winner FROM nobel WHERE winner LIKE 'John %'
+
+SELECT yr, subject, winner FROM nobel
+  WHERE (subject = 'Physics' AND yr = 1980)
+  OR (subject = 'Chemistry' AND yr = 1984);
+
+SELECT yr, subject, winner FROM nobel WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine');
+
+SELECT yr, subject, winner FROM nobel
+  WHERE (subject = 'Medicine' AND yr < 1910)
+  OR (subject = 'Literature' AND yr >= 2004);
+
+SELECT * FROM nobel WHERE winner = 'Peter Grünberg';
+
+SELECT * FROM nobel WHERE winner = "Eugene O\'neill";
+
+SELECT winner, yr, subject FROM nobel WHERE winner LIKE 'Sir%' ORDER BY yr DESC, winner;
+
+SELECT winner, subject
+  FROM nobel
+  WHERE yr=1984
+  ORDER BY subject IN ('Physics','Chemistry'), subject, winner;
+
