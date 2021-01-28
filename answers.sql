7d2dcd4e3fc2bcb0feb71a09c3987f9a2c7e1b04
AymenBida <aymen.bida.00@gmail.com>
2021-01-28 14:41:34 +0100

    Complete Exercice 6

--- answers.sql
+++ answers.sql
@@ -150,3 +150,23 @@ SELECT name, continent, population FROM world x
 SELECT name, continent FROM world x
   WHERE population / 3 > ALL(SELECT population FROM world y WHERE x.continent = y.continent AND x.name != y.name);
 
+--Exercice 6:
+
+SELECT SUM(population) FROM world;
+
+SELECT DISTINCT continent FROM world;
+
+SELECT SUM(gdp) FROM world WHERE continent = 'Africa';
+
+SELECT COUNT(name) FROM world WHERE area >= 1000000;
+
+SELECT SUM(population) FROM world WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
+
+SELECT continent, COUNT(name) FROM world GROUP BY continent;
+
+SELECT continent, COUNT(name) FROM world WHERE population >= 10000000 GROUP BY continent;
+
+SELECT continent FROM world GROUP BY continent HAVING SUM(population) >= 100000000;
+
+--Exercice 7:
+
