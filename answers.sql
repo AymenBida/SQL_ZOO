085deef14c2cef28c4b5c2d5f134ed8236c2bfb4
AymenBida <aymen.bida.00@gmail.com>
2021-01-29 09:31:39 +0100

    Complete Exercice 10

--- answers.sql
+++ answers.sql
@@ -291,3 +291,31 @@ SELECT teacher.name,
   FROM teacher LEFT JOIN dept ON (teacher.dept = dept.id);
 
 --Exercice 10:
+
+SELECT A_STRONGLY_AGREE FROM nss
+  WHERE question = 'Q01' AND institution = 'Edinburgh Napier University' AND subject = '(8) Computer Science';
+
+SELECT institution, subject FROM nss WHERE question = 'Q15' AND score >= 100;
+
+SELECT institution, score FROM nss
+  WHERE question = 'Q15' AND subject = '(8) Computer Science' AND score < 50;
+
+SELECT subject, SUM(response) FROM nss
+  WHERE question = 'Q22' AND subject IN ('(8) Computer Science', '(H) Creative Arts and Design') GROUP BY subject;
+
+SELECT subject, SUM(response * A_STRONGLY_AGREE / 100) FROM nss
+  WHERE question = 'Q22' AND subject IN ('(8) Computer Science', '(H) Creative Arts and Design') GROUP BY subject;
+
+SELECT subject, ROUND(SUM(response * A_STRONGLY_AGREE / 100) / SUM(response) * 100) FROM nss
+  WHERE question = 'Q22' AND subject IN ('(8) Computer Science', '(H) Creative Arts and Design') GROUP BY subject;
+
+SELECT institution, ROUND(SUM(response * score / 100) / SUM(response) * 100) FROM nss
+  WHERE question = 'Q22' AND (institution LIKE '%Manchester%') GROUP BY institution;
+
+SELECT institution,
+  SUM(sample),
+  (SELECT sample FROM nss x WHERE subject = '(8) Computer Science' AND x.institution = y.institution AND question = 'Q01')
+  FROM nss y
+  WHERE question = 'Q01' AND (institution LIKE '%Manchester%') GROUP BY institution;
+
+--Exercice 11:
