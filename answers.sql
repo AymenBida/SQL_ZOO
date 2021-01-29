59595126343f72a17f8f593c7293b239e99440b9
AymenBida <aymen.bida.00@gmail.com>
2021-01-29 09:57:30 +0100

    Complete Exercice 11

--- answers.sql
+++ answers.sql
@@ -319,3 +319,32 @@ SELECT institution,
   WHERE question = 'Q01' AND (institution LIKE '%Manchester%') GROUP BY institution;
 
 --Exercice 11:
+
+SELECT lastName, party, votes FROM ge WHERE constituency = 'S14000024' AND yr = 2017 ORDER BY votes DESC;
+
+SELECT party, votes, RANK() OVER (ORDER BY votes DESC) as posn FROM ge
+  WHERE constituency = 'S14000024' AND yr = 2017 ORDER BY party;
+
+SELECT yr, party, votes, RANK() OVER (PARTITION BY yr ORDER BY votes DESC) as posn FROM ge
+  WHERE constituency = 'S14000021' ORDER BY party, yr;
+
+SELECT constituency,party, votes, RANK() OVER (PARTITION BY constituency ORDER BY votes DESC) rank FROM ge
+  WHERE constituency BETWEEN 'S14000021' AND 'S14000026' AND yr  = 2017 ORDER BY rank, constituency;
+
+SELECT constituency, party
+  FROM (
+    SELECT constituency, party, votes, RANK() OVER (PARTITION BY constituency ORDER BY votes DESC) rank
+    FROM ge
+    WHERE constituency BETWEEN 'S14000021' AND 'S14000026'
+    AND yr  = 2017
+    ORDER BY rank, constituency) AS x
+  WHERE rank = 1;
+
+SELECT party, COUNT(votes) FROM ge x
+  WHERE constituency LIKE 'S%'
+  AND yr = 2017
+  AND votes >= ALL(SELECT votes FROM ge y WHERE x.constituency = y.constituency AND y.yr = 2017)
+  GROUP BY party;
+
+--Exercice 12:
+
